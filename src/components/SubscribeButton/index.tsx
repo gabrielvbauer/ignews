d5b8e9c60e45b070ed738df252f2a486279ffe66
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-08-30 21:08:42 -0300

    feat: stripe checkout redirection

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,13 +10,21 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const {data} = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!data) {
       signIn('github')
       return;
     }
 
-    
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId: sessionId })
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   return (
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,17 +1,53 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { fauna } from '../../services/fauna';
+import { query } from "faunadb";
 import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
 
-export default async (request: NextApiRequest, response: NextApiResponse) => {
-  if (request.method === 'POST') {
-    const session = await getSession({ req: request })
+type User = {
+  ref: {
+    id: string;
+  },
+  data: {
+    stripe_customer_id: string;
+  }
+}
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email
-    })
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({req})
+
+    const user = await fauna.query<User>(
+      query.Get(
+        query.Match(
+          query.Index('user_by_email'),
+          query.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id;
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      })
+
+      await fauna.query(
+        query.Update(
+          query.Ref(query.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id;
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
@@ -23,9 +59,9 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL
     })
 
-    return response.status(200).json({ sessionId: stripeCheckoutSession.id });
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
   } else {
-    response.setHeader('Allow', 'POST');
-    response.status(405).end('Method not allowed');
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed');
   }
 }
