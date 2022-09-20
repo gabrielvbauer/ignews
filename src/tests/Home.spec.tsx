238d8170268b715715dc2f8241b001c8089f7410
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-09-20 19:51:27 -0300

    feat: getStaticProps tests

--- src/tests/Home.spec.tsx
+++ src/tests/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../pages';
+import { stripe } from '../services/stripe'
+import Home, { getStaticProps } from '../pages';
 
 jest.mock('next/router', () => {
   return {
@@ -16,6 +17,7 @@ jest.mock('next-auth/react', () => {
     }
   }
 })
+jest.mock('../services/stripe')
 
 describe('Home page', () => {
   it('should render correctly', () => {
@@ -29,5 +31,26 @@ describe('Home page', () => {
     )
 
     expect(screen.getByText('for R$10,00 month')).toBeInTheDocument()
+  })
+
+  it('should load inital data', async () => {
+    const retrieveStripeMocked = jest.mocked(stripe.prices.retrieve)
+    retrieveStripeMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000
+    } as any)
+
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00'
+          }
+        }
+      })
+    )
   })
 })
